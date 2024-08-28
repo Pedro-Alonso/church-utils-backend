2036a1c7c4f2448423e5ae3fb43ec967162c511a
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-28 11:00:33 -0300

    🔧 Port and host update

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app.register_blueprint(helloworld.helloworld_bp)
 app.register_blueprint(lyrics.lyrics_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, host="0.0.0.0")
